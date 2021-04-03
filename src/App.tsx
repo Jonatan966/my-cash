c4ff2b23b5f3a6cc61158fda86b74790a0d980e7
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-04-03 13:08:17 -0300

    feat: transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from 'react-modal'
+
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+
   return (
     <>
-       <Header/>
-       <Dashboard/>
-       <GlobalStyle/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard/>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle/>
     </>
   );
 }
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+
+import { Container, RadioBox, TransactionTypeContainer } from './styles'
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt="Fechar modal"/>
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input 
+          placeholder='Título'
+        />
+        <input 
+          type="number" 
+          placeholder='Valor'
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type='button'
+            isActive={type === 'deposit'}
+            onClick={() => setType('deposit')}
+            activeColor='green'
+          >
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type='button'
+            isActive={type === 'withdraw'}
+            onClick={() => setType('withdraw')}
+            activeColor='red'
+          >
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input 
+          placeholder='Categoria'
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  );
+}
