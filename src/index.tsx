2a55b376404ecdcb21d19ad847bee1309e93160e
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-04-09 15:00:30 -0300

    fix: service worker adjusts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
 import { App } from './App';
-import { registerServiceWorker } from './serviceWorker';
 
 
 ReactDOM.render(
@@ -12,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-registerServiceWorker()
+ServiceWorkerRegistration.register()
