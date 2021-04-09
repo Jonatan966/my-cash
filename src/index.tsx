fd923c7e349c234891296a7d0e0e1626269ebb86
Jonatan Frederico <37812781+Jonatan966@users.noreply.github.com>
2021-04-09 13:57:44 -0300

    feat: pwa implementation

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { App } from './App';
+import { registerServiceWorker } from './serviceWorker';
 
 
 ReactDOM.render(
@@ -10,3 +11,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerServiceWorker()
