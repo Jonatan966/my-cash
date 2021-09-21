f371fa098822a94a9a4b024bd09b5effb3c42767
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-21 17:00:57 -0300

    refact: put theme switcher in a separate component

--- src/pages/ListingPage/index.tsx
+++ src/pages/ListingPage/index.tsx
@@ -1,45 +1,20 @@
-import { useContext } from 'react'
-import { ThemeContext } from 'styled-components'
-import Switch from 'react-switch'
-import { lighten } from 'polished'
-
-import moonImg from '../../assets/moon.svg'
-import sunImg from '../../assets/sun.svg'
-import logoImg from '../../assets/logo.svg'
-
 import { NavigationBar } from 'components/NavigationBar'
 import { Summary } from 'components/Summary'
 import { TransactionsTable } from 'components/TransactionsTable'
+import { ThemeSwitcher } from 'components/ThemeSwitcher'
 
-import { useThemeSwitcher } from 'hooks/useThemeSwitcher'
+import logoImg from 'assets/logo.svg'
 
 import { Container, HeaderContainer, HeaderContent } from './styles'
 
 export function ListingPage() {
-  const { title } = useContext(ThemeContext)
-  const { toggleTheme } = useThemeSwitcher()
-
   return (
     <>
       <HeaderContainer>
         <HeaderContent>
           <img src={logoImg} alt="dt.money" />
 
-          <Switch
-            checked={title === 'light'}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            checkedHandleIcon={<img src={sunImg} alt="Light mode" />}
-            uncheckedHandleIcon={<img src={moonImg} alt="Dark mode" />}
-            onColor="#f4a261"
-            offColor="#363f5f"
-            onHandleColor={lighten(0.25, '#f4a261')}
-            offHandleColor={lighten(0.25, '#363f5f')}
-            handleDiameter={30}
-            height={20}
-            onChange={toggleTheme}
-            className="switcher"
-          />
+          <ThemeSwitcher />
         </HeaderContent>
       </HeaderContainer>
       <Container>
