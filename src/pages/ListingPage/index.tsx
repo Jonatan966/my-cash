d27d2c3e00d955c874676c602c69d0f321d492bf
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-21 16:55:52 -0300

    refact: put header into listing page

--- src/pages/ListingPage/index.tsx
+++ src/pages/ListingPage/index.tsx
@@ -1,19 +1,52 @@
-import { Header } from 'components/Header'
+import { useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+import Switch from 'react-switch'
+import { lighten } from 'polished'
+
+import moonImg from '../../assets/moon.svg'
+import sunImg from '../../assets/sun.svg'
+import logoImg from '../../assets/logo.svg'
+
 import { NavigationBar } from 'components/NavigationBar'
 import { Summary } from 'components/Summary'
 import { TransactionsTable } from 'components/TransactionsTable'
 
-import { Container } from './styles'
+import { useThemeSwitcher } from 'hooks/useThemeSwitcher'
+
+import { Container, HeaderContainer, HeaderContent } from './styles'
 
 export function ListingPage() {
+  const { title } = useContext(ThemeContext)
+  const { toggleTheme } = useThemeSwitcher()
+
   return (
     <>
-      <Header />
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={logoImg} alt="dt.money" />
+
+          <Switch
+            checked={title === 'light'}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            checkedHandleIcon={<img src={sunImg} alt="Light mode" />}
+            uncheckedHandleIcon={<img src={moonImg} alt="Dark mode" />}
+            onColor="#f4a261"
+            offColor="#363f5f"
+            onHandleColor={lighten(0.25, '#f4a261')}
+            offHandleColor={lighten(0.25, '#363f5f')}
+            handleDiameter={30}
+            height={20}
+            onChange={toggleTheme}
+            className="switcher"
+          />
+        </HeaderContent>
+      </HeaderContainer>
       <Container>
         <Summary />
         <TransactionsTable />
       </Container>
-      <NavigationBar />
+      <NavigationBar selectedRoute={'listing'} />
     </>
   )
 }
--- src/pages/ListingPage/styles.ts
+++ src/pages/ListingPage/styles.ts
@@ -5,3 +5,57 @@ export const Container = styled.main`
   margin: 0 auto;
   padding: 2.5rem 1rem;
 `
+
+export const HeaderContainer = styled.header`
+  background: ${ctx => ctx.theme.colors.primary};
+`
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #fff;
+    background: ${ctx => ctx.theme.colors.primaryLight};
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    grid-area: btnTransaction;
+    
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+
+  .switcher {
+    grid-area: btnSwitcher;
+  }
+
+  > img {
+    grid-area: logo;
+  }
+
+  @media (max-width: 375px) {
+    > img {
+      zoom: 0.8;
+    }
+  }
+
+  @media (max-width: 820px) {
+    display: grid;
+    grid-template-areas:
+        "logo btnSwitcher"
+        "btnTransaction btnTransaction";
+    gap: 1.5rem;
+  }
+`
