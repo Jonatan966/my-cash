6db1025468f5d41b1cc23bd80df1683e2459d81a
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-10-30 18:41:21 -0300

    feat: create auth context

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,93 @@
+import { useContext, useEffect, useState, createContext, ReactNode } from 'react'
+import { useHistory } from 'react-router'
+
+import { User } from 'interfaces/User'
+import { authApp, authConfig } from 'services/firebase'
+
+export type TargetProviders = 'google' | 'facebook' | 'anonymous'
+
+interface AuthContextProps {
+  signIn: (targetProvider: TargetProviders) => Promise<void>;
+  signOut: () => Promise<void>;
+  user: User | undefined;
+  isLoadingUserInformation: boolean;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext({} as AuthContextProps)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
+  const [isLoadingUserInformation, setIsLoadingUserInformation] = useState(true)
+  const router = useHistory()
+
+  useEffect(() => {
+    setIsLoadingUserInformation(true)
+
+    const unsubscribe = authApp.onAuthStateChanged(authConfig, user => {
+      if (user) {
+        saveUserInformation(user)
+      }
+      setIsLoadingUserInformation(false)
+    })
+
+    return () => unsubscribe()
+  }, [])
+
+  async function signIn(targetProvider: TargetProviders) {
+    const providers = {
+      google: new authApp.GoogleAuthProvider(),
+      facebook: new authApp.FacebookAuthProvider(),
+    }
+
+    let signInResult = null
+
+    if (targetProvider === 'anonymous') {
+      signInResult = await authApp.signInAnonymously(authConfig)
+    } else {
+      signInResult = await authApp.signInWithPopup(authConfig, providers[targetProvider])
+    }
+
+    if (signInResult.user) {
+      saveUserInformation(signInResult.user)
+
+      router.replace('/')
+    }
+  }
+
+  async function signOut() {
+    await authApp.signOut(authConfig)
+    setUser(undefined)
+    router.replace('/auth')
+  }
+
+  function saveUserInformation(userInformation: authApp.User) {
+    const { displayName, photoURL, uid, isAnonymous } = userInformation
+
+    if ((!displayName || !photoURL) && !isAnonymous) {
+      throw new Error('Missing information from account.')
+    }
+
+    setUser({
+      id: uid,
+      name: displayName || 'Anonymous',
+      avatar: photoURL || ''
+    })
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      user,
+      signIn,
+      signOut,
+      isLoadingUserInformation
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
