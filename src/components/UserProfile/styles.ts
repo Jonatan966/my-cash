2d29447f08deca6fbdc64e0343c39bb85b8ae4ee
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-10-30 21:51:54 -0300

    feat: create user profile component

---
+++ src/components/UserProfile/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const UserProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  img {
+    width: 3.5rem;
+    height: 3.5rem;
+
+    border-radius: 0.5rem;
+  }
+
+  h3 {
+    font-weight: normal;
+
+    color: #fff;
+  }
+`
