557960e985daeed62b4c75a2d711d4d7c3a53008
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-01-22 14:02:21 -0300

    feat: implement spinner lib

---
+++ src/components/BackdropLoader/index.tsx
@@ -0,0 +1,19 @@
+import ClipLoader from 'react-spinners/ClipLoader'
+import { useTheme } from 'styled-components'
+
+import { Container } from './styles'
+
+interface BackdropLoaderProps {
+  fullScreen?: boolean,
+}
+
+export function BackdropLoader({ fullScreen }: BackdropLoaderProps) {
+  const { colors } = useTheme()
+
+  return (
+    <Container fullScreen={fullScreen}>
+      <ClipLoader color={colors.primary} size={128} />
+      <h2>Obtendo dados. . .</h2>
+    </Container>
+  )
+}
