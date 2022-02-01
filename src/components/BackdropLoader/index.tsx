c4d9ae2cd792da35d0c689919b84eacfdd660614
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-01 20:59:51 -0300

    feat: customize backdrop loader

--- src/components/BackdropLoader/index.tsx
+++ src/components/BackdropLoader/index.tsx
@@ -1,19 +1,25 @@
 import ClipLoader from 'react-spinners/ClipLoader'
-import { useTheme } from 'styled-components'
+import { DefaultTheme, useTheme } from 'styled-components'
 
 import { Container } from './styles'
 
 interface BackdropLoaderProps {
   fullScreen?: boolean,
+  title?: string,
+  color?: keyof DefaultTheme['colors'],
 }
 
-export function BackdropLoader({ fullScreen }: BackdropLoaderProps) {
+export function BackdropLoader({
+  fullScreen,
+  title = 'Obtendo dados. . .', 
+  color = 'green',
+}: BackdropLoaderProps) {
   const { colors } = useTheme()
 
   return (
     <Container fullScreen={fullScreen}>
-      <ClipLoader color={colors.green} size={128} />
-      <h2>Obtendo dados. . .</h2>
+      <ClipLoader color={colors[color]} size={128} />
+      <h2>{title}</h2>
     </Container>
   )
 }
