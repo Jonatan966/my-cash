b54d3b10d9ae05f644de43549cfa1081293e5df4
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-02 21:03:36 -0300

    refactor: remove dexie

--- src/hooks/useDatabase.ts
+++
@@ -1,9 +0,0 @@
-import { DtMoneyDatabase } from 'services/DexieDatabase'
-
-const database = new DtMoneyDatabase()
-
-export function useDatabase() {
-  return {
-    transactionsTable: database.transactions
-  }
-}
