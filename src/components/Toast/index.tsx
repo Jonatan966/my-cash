48c880aed68ce6443b842e68b6b9a1bd7bec7669
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-06-07 10:20:47 -0300

    fix: remove duplicated files

--- src/components/Toast/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ToastContainer } from 'react-toastify'
-import { useTheme } from 'styled-components'
-import 'react-toastify/dist/ReactToastify.css'
-
-export function Toast() {
-  const { title } = useTheme()
-
-  return <ToastContainer theme={title} />
-}
--- src/interfaces/Transactions.ts
+++
@@ -1,9 +0,0 @@
-export interface ITransaction {
-  id?: string
-  title: string
-  amount: number
-  category: string
-  transactionDate: string
-  type: 'deposit' | 'withdraw'
-  createdAt: number
-}
