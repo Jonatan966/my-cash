2c0c1598f9595d2d75d535a9218deef55729bd96
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-06-06 23:13:57 -0300

    refactor: separate transaction type component

--- src/domain/TransactionModal/styles.ts
+++ src/domain/TransactionModal/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
   display: flex;
@@ -17,48 +16,5 @@ export const Container = styled.form`
 
     padding: 0 1.5rem;
     margin-top: 1.5rem;
-  }
-`
-
-export const TransactionTypeContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 0.5rem;
-`
-
-interface RadioBoxProps {
-  isActive: boolean
-  activeColor: 'green' | 'red'
-}
-
-export const RadioBox = styled.button<RadioBoxProps>`
-  height: 3rem;
-  border: 1.5px solid ${(ctx) => ctx.theme.colors.inputBorder};
-  border-radius: 0.25rem;
-
-  background: ${(ctx) =>
-    ctx.isActive
-      ? transparentize(0.8, ctx.theme.colors[ctx.activeColor])
-      : 'transparent'};
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  transition: all 0.2s;
-
-  &:hover {
-    border-color: ${(ctx) => darken(0.2, ctx.theme.colors[ctx.activeColor])};
-  }
-
-  span {
-    display: inline-block;
-    margin-left: 1rem;
-    font-size: 1rem;
-    color: ${(ctx) => ctx.theme.colors.textTitle};
-  }
-
-  svg {
-    color: ${(ctx) => ctx.theme.colors[ctx.activeColor]};
   }
 `
